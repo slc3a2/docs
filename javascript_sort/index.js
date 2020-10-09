0839dbeb8a201f18ac116d98fd125aee2dccff5f
李良奇 <codedance98@gmail.com>
2020-10-09 17:21:24 +0800

    update 2 sort-methods

--- javascript_sort/index.js
+++ javascript_sort/index.js
@@ -20,12 +20,16 @@ function bubbleSort(arr) {
 // 选择排序(Selection Sort)
 function selectionSort(arr) {
   let minIndex = 0; // 最小值下标，默认可设置为0
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < arr[minIndex]) {
-      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];// 使用es6结构语法实现值互换
-      minIndex = i;
+  for (let j = 0; j < arr.length - 1; j++) {// arr.length - 1是因为最后2个值完成比较后，最后一位只剩他自己了，就不需要排序了。
+    var minVal = arr[i];
+    for (let i = j+1; i < arr.length - 1; i++) {
+      if (arr[i] < minVal) {
+        [arr[i], minVal] = [minVal, arr[i]];// 使用es6结构语法实现值互换
+        minIndex = i;
+      }
     }
   }
+  return arr;
   console.log(arr[minIndex])
   console.log(arr)
   console.log(minIndex)
