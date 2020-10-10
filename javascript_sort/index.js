51a4296bfa0e97b89d55cf49288a1bb6d9cc6b41
李良奇 <codedance98@gmail.com>
2020-10-10 15:41:25 +0800

    up

--- javascript_sort/index.js
+++ javascript_sort/index.js
@@ -19,14 +19,14 @@ function bubbleSort(arr) {
 
 // 选择排序(Selection Sort)
 function selectionSort(arr) {
-  for (let j = 0; j < arr.length - 1; j++) { // arr.length - 1是因为最后2个值完成比较后，最后一位只剩他自己了，就不需要排序了。
-    let minVal = arr[j];
-    for (let i = j+1; i < arr.length; i++) {
-      if (arr[i] < minVal) {
-        [arr[i], minVal] = [minVal, arr[i]]; // 使用es6结构语法实现值互换
+  for (let i = 0; i < arr.length - 1; i++) { // arr.length - 1是因为假定数组中n个数，比较需要两个数，所以只需要比较n-1次
+    let minIndex = i; // 记录每次循环开始位置，等待和最小值交换位置
+    for (let j = i+1; j < arr.length; j++) { // j+1 是避免比较自身，j永远是i的后一项，让他们俩做比较
+      if (arr[j] < arr[minIndex]) { // 如果后面的值小于前面的值，需要调换他们俩位置
+        minIndex = j; // 保存最小值的下标
       }
     }
-    arr[j] = minVal; // 把最小值放到数组最前面
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]] // 使用es6的结构语法交换值，把最小值放到数组最前面
   }
   return arr;
 }
