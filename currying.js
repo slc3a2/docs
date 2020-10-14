72c796cf1d628c46ac598ea501ab19b088f53d05
李良奇 <codedance98@gmail.com>
2020-10-14 19:22:44 +0800

    create currying function demo

---
+++ currying.js
@@ -0,0 +1,8 @@
+function currying (fn) {
+  var args = [].slice.call(arguments, 1);
+  return function() {
+      var newArgs = args.concat([].slice.call(arguments));
+      return fn.apply(this, newArgs);
+  };
+};
+
