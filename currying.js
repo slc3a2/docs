b8be39b81c86d4f80ab65e1f12eed290e6cb1101
李良奇 <codedance98@gmail.com>
2020-10-16 19:20:45 +0800

    update

--- currying.js
+++ currying.js
@@ -1,8 +1,23 @@
-function currying (fn) {
-  var args = [].slice.call(arguments, 1);
+function curry(fn, args) {
+  var length = fn.length;
+  args = args || [];
   console.log(args)
   return function() {
-      var newArgs = args.concat([].slice.call(arguments));
-      return fn.apply(this, newArgs);
-  };
+    var _args = args.slice(0)
+    var arg;
+    for (let i = 0; i < arguments.length; i++) {
+        arg = arguments[i];
+        _args.push(arg);
+    }
+    if (_args.length < length) {
+        return curry.call(this, fn, _args);
+    }
+    else {
+        return fn.apply(this, _args);
+    }
+  }
 }
+
+var fn = curry(function(a, b, c) {
+  console.log([a, b, c]);
+});
