2f83d67bf5c6acc47dc9776e3923adef54fd055b
李良奇 <codedance98@gmail.com>
2021-01-07 11:02:14 +0800

    update: 更新lc

--- practice/linked_list.js
+++ practice/linked_list.js
@@ -8,7 +8,6 @@ class Node {
 class LinkedList {
   constructor() {
     this.head = new Node('head');
-    return this.print();
   }
   find(item) {
     let curNode = this.head;
@@ -40,6 +39,7 @@ class LinkedList {
       temp.push(t.value);
       t = t.next;
     }
+    console.log(temp.join('->'))
     return temp.join('->')
   }
 }
@@ -47,6 +47,28 @@ let linked = new LinkedList();
 linked.insert('1')
 linked.insert('2')
 linked.insert('3')
-linked.print()
-linked.remove('2')
-linked.find('1')
+linked.print();
+// linked.remove('2')
+// linked.find('1')
+
+var reverseList = function(head) {
+  if(head === null || head.next === null){return head;}
+  let prev = null; // 临时存储
+  let cur = head; // 起点位置
+  // // 从链表起点循环到结尾
+  // while(curr != null) {
+  //   // 保存断开之后的节点
+  //   let nextTemp = curr.next; 
+  //   curr.next = prev; // head指向null
+  //   prev = curr; // prev改变到修改后的列表的头部
+  //   curr = nextTemp; // 取出下一个要处理的节点
+  // }
+  let temp = cur.next;
+  cur.next = prev;
+  prev = cur;
+  cur = temp
+  
+}
+
+let res = reverseList(linked.head);
+console.log(res)
