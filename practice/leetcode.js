48fded7ae058e61ae9a6471047c7244ad7814796
李良奇 <codedance98@gmail.com>
2021-01-11 18:48:29 +0800

    update: 练习题

--- practice/leetcode.js
+++ practice/leetcode.js
@@ -132,33 +132,78 @@
 //   return arr;
 // }
 
-// 206. 反转链表
-var reverseList = function(head) {
-  console.log(head)
-  // if(head.next === null || head == null){return head;}
-  // let prev = null;
-  // while(head) {
-  //   let next = head.next;
-  //   head.next = pre;
-  //   pre = head;
-  //   head = next;
-  // }
-  // return pre;
-  console.log(head.next)
-  console.log(head.next.next)
-  head.next.next = head;
-  console.log(head)
-  head.next = null;
-  console.log(head);
-  return head;
-  // let next = head.next;
-  // head.next = null;
-};
-let temp = {
-  key: 1,
-  next: {
-    key: 2,
-    next: null
-  }
-}
-reverseList(temp)
+// 206. 反转链表  ✅
+// var reverseList = function(head) {
+//   // 将处理的链表
+//   let todo_linked = head;
+//   // 新的链表
+//   let new_linked = null;
+//   while(todo_linked) {
+//     // 拿到要处理的链表下一节点
+//     let next = todo_linked.next;
+//     // 切换，把todo_linked下一节点设置为新链表
+//     todo_linked.next = new_linked;
+//     // 设置完，更新new_linked，留着下次替换使用
+//     new_linked = todo_linked;
+//     // 接着处理next的下一项
+//     todo_linked = next;
+//   }
+//   return new_linked;
+// };
+// let temp = {
+//   key: 1,
+//   next: {
+//     key: 2,
+//     next: null
+//   }
+// }
+// reverseList(temp)
+
+// 165. 比较版本号  ✅
+// var compareVersion = function(version1, version2) {
+//   let v1 = version1.split('.');
+//   let v2 = version2.split('.');
+//   for(let i = 0; i < Math.max(v1.length, v2.length); i++){
+//     if(v1[i] === undefined){
+//       v1.push('0')
+//     }
+//     if(v2[i] === undefined){
+//       v2.push('0')
+//     }
+//     v1[i] = Number(v1[i])
+//     v2[i] = Number(v2[i])
+//   }
+//   let v1_t = Number(v1.join(''))
+//   let v2_t = Number(v2.join(''))
+//   if(v1_t > v2_t){
+//     return 1
+//   }else if(v1_t < v2_t){
+//     return -1
+//   }else{
+//     return 0
+//   }
+// };
+// console.log(compareVersion('1.01', '1.001'));
+// console.log(compareVersion('1.0', '1.0.0'));
+// console.log(compareVersion('0.1', '1.1'));
+
+// 1. 两数之和 ✅
+// var twoSum = function(nums, target) {
+//   let temp = [];
+//   for(let i = 0; i < nums.length; i++){
+//     for(let j = 0; j < nums.length; j++){
+//       if(nums[i] + nums[j] === target){
+//         if(i === j){continue;}
+//         temp.push(i)
+//         temp.push(j)
+//       }
+//     }
+//   }
+//   let uni = Array.from(new Set(temp));
+//   return uni
+// };
+
+// twoSum([2,7,11,15], 9)
+// twoSum([3,2,4], 6)
+// twoSum([3,3], 6)
+
