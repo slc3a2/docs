3e94c6e308837a5992a12df3ec099777382e8e75
李良奇 <codedance98@gmail.com>
2021-01-20 10:34:08 +0800

    update: 练习题

--- practice/leetcode.js
+++ practice/leetcode.js
@@ -317,19 +317,33 @@
 // console.log(lengthOfLongestSubstring("au"))
 
 // 7. 整数反转 ✅ 
-var reverse = function(x) {
-  let res = 0;
-  while(x !== 0){
-    res = res * 10 + x % 10;
-    x = parseInt(x / 10)
+// var reverse = function(x) {
+//   let res = 0;
+//   while(x !== 0){
+//     res = res * 10 + x % 10;
+//     x = parseInt(x / 10)
+//   }
+//   if(res > (Math.pow(2, 31) - 1) || res < -(Math.pow(2, 31))) {
+//     res = 0;
+//   }
+//   return res;
+// };
+
+// reverse(123)
+// reverse(-123)
+// reverse(120)
+// reverse(0)
+
+var thousandSeparator = function(n) {
+  let temp = String(n);
+  let res;
+  for(let i = temp.length - 3; i === 0; i--){
+    console.log(i)
+    // res = temp.slice(0, i) + '.' + temp.slice(i);
   }
-  if(res > (Math.pow(2, 31) - 1) || res < -(Math.pow(2, 31))) {
-    res = 0;
-  }
+  // console.log(res)
   return res;
 };
 
-reverse(123)
-reverse(-123)
-reverse(120)
-reverse(0)
+// thousandSeparator(987)
+thousandSeparator(12345678)
