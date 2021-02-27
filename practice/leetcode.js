e94e53b4d85028cab3d9635a96b1b591000a23fb
李良奇 <codedance98@gmail.com>
2021-02-27 16:13:20 +0800

    update: 更新练习题

--- practice/leetcode.js
+++ practice/leetcode.js
@@ -437,54 +437,48 @@
 // };
 
 // 42. 接雨水
-// 找到数组最高节点，以此向左右节点遍历，如果下一个节点值比当前节点值小，那么说明下一个节点可以接到水, 执行接水方法，然后递归这个方法，只到满为止
+// 从左边第一个节点开始遍历，分别找到符合条件的他的前一项和后一项，要求是满足:前一项 > 当前值 < 后一项，如不满足，前一项继续向前移动一位，后一项向后移动一位，
+// 如果有符合条件的组合下：如果left值大于right值，所有在left和right之间的值需要等于right的值，否则left和right之间的值都等于left的值，然后每次变化，用count去记录，最后返回count，count为结果
 var trap = function(height) {
   let count = 0;
   for(let i = 0; i < height.length; i++) {
     let item = height[i];
     let left, right;
+    let startIdx, endIdx
     for(let j = i; j >= 0; j--) {
       if(height[j] > item) {
         left = height[j]
+        startIdx = j;
         break;
       }
     }
     for(let l = i; l < height.length; l++) {
       if(height[l] > item) {
         right = height[l]
+        endIdx = l;
         break;
       }
     }
-    // console.log(left, item, right)
     if(typeof(right) !== 'undefined' && typeof(left) !== 'undefined' && typeof(item) !== 'undefined') {
-      // console.log('符合条件进入判断')
       let temp = 0;
       if(left > right) {
-        temp = left - right
-      } else if(left < right) {
-        temp = right - left;
+        temp = right;
       } else {
-        temp = right - item;
+        temp = left;
       }
       if(temp !== 0) {
-        console.log(height.indexOf(left), height.indexOf(right))
-        for(let ii = height.indexOf(left)+1; ii < height.indexOf(right); ii++) {
+        for(let ii = startIdx+1; ii < endIdx; ii++) {
           count += temp - height[ii];
-          // let temp1 = height[ii];
-          // console.log(ii)
-          // height[ii] += temp
-          // // console.log(height[ii], temp1)
-          // count += height[ii] - temp1;
-          // console.log('---')
+          height[ii] = temp;
         }
       }
     }
   }
-  console.log(count)
-  console.log(height)
+  return count;
 };
 
 // trap([4,3,2,1,2,3,4])
+// trap([0,1,0,2,1,0,1,3,2,1,2,1])
 trap([0,1,0,2,1,0,1,3,2,1,2,1])
 
 
