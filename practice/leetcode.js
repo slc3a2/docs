05864e6b724017fe3622ce72317dfd507624d85e
李良奇 <codedance98@gmail.com>
2021-03-06 17:55:21 +0800

    update: 练习题

--- practice/leetcode.js
+++ practice/leetcode.js
@@ -581,3 +581,16 @@
 // let num1 = "123"; let num2 = "456";
 // multiply(num1, num2)
 
+// 26. 删除排序数组中的重复项
+var removeDuplicates = function(nums) {
+  for(let i = 0; i < nums.length - 1; i++) {
+    if(nums[i] === nums[i+1]) {
+      nums.splice(i,1)
+      i--;
+    }
+  }
+  return nums.length
+};
+removeDuplicates([0,0,0,1,2,3])
+removeDuplicates([0,0,1,1,1,2,2,3,3,4])
+
