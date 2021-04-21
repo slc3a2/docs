67ee8d3ecd7fafd168d74a7b7b6586e5a78f62ef
李良奇 <codedance98@gmail.com>
2021-04-21 19:10:25 +0800

    Update: 更新练习题

--- practice/leetcode.js
+++ practice/leetcode.js
@@ -378,17 +378,17 @@
 // romanToInt(20)
 
 // 4. 寻找两个正序数组的中位数 ❌ leetcode[1,3] [2]用例和本地结果不同
-// var findMedianSortedArrays = function(nums1, nums2) {
-//   let temp = nums1.concat(nums2).sort((a, b)=> {return a > b});
-//   console.log(temp)
-//   if(temp.length % 2 === 1) {
-//     return temp[temp.length / 2 - 0.5]
-//   } else {
-//     let prev = temp[temp.length / 2 - 1];
-//     let next = temp[temp.length / 2];
-//     return (prev + next) / 2
-//   }
-// };
+var findMedianSortedArrays = function(nums1, nums2) {
+  let temp = nums1.concat(nums2).sort((a, b)=> {return a - b});
+  let len = temp.length;
+  if(len % 2 === 1) {
+    return temp[len / 2 - 0.5]
+  } else {
+    let prev = temp[len / 2 - 1];
+    let next = temp[len / 2];
+    return (prev + next) / 2
+  }
+};
 
 // nums1 = [1, 3], nums2 = [2]
 // console.log(findMedianSortedArrays(nums1, nums2))
