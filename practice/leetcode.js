e7aa12f05c00841f8c949a6e76edc34562c034f0
李良奇 <codedance98@gmail.com>
2021-05-03 23:54:38 +0800

    Update: 练习题

--- practice/leetcode.js
+++ practice/leetcode.js
@@ -1245,3 +1245,37 @@ for(let i = 0, len = r2.length; i < len; i++) {
     // console.log(r2[i])
   }
 }
+// 415. 字符串相加
+let addStrings = function(a, b) {
+  let res = []
+  let temp = 0
+  if(a.length > b.length) {
+    let t = a.length - b.length
+    for(let i = 0; i < t; i++) {
+      b = '0' + b
+    }
+  } else {
+    let t = b.length - a.length
+    for(let i = 0; i < t; i++) {
+      a = '0' + a
+    }
+  }
+  for(let len = Math.max(a.length, b.length) - 1; len >= 0; len--) {
+      let i = len
+      let t_a = Number(a.charAt(i) || 0)
+      let t_b = Number(b.charAt(i) || 0)
+      // console.log(t_a , t_b , temp)
+      let count = String(t_a + t_b + temp)
+      temp = 0
+      if(count.length > 1) {
+        temp = Number(count.charAt(0))
+        res.push(count.charAt(1))
+      } else {
+        res.push(count)
+      }
+  }
+  if(temp !== 0) {
+    res.push(temp)
+  }
+  return res.reverse().join('')
+}
