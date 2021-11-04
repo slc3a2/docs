2ddcc47847dd3c58553cbc1f2239ef315d260a85
李良奇 <codedance98@gmail.com>
2021-11-05 00:09:14 +0800

    add: 104. 二叉树的最大深度

---
+++ lt/104. 二叉树的最大深度.js
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxDepth = function(root) {
+  if(!root) {
+    return 0
+  }
+  let res = 0
+  const queue = [root]
+  while(queue.length) {
+    let level = []
+    for(let i = 0, len = queue.length; i < len; i++) {
+      let {left, right} = queue.shift()
+      if(left) queue.push(left)
+      if(right) queue.push(right)
+    }
+    if(level.length > 0) {
+      res++
+    }
+  } 
+  return res
+};
+let tree = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 4,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 5,
+      left: null,
+      right: null,
+    }
+  },
+  right: {
+    val: 3,
+    left: {
+      val: 6,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 7,
+      left: null,
+      right: null,
+    }
+  }
+}
+
+console.log(maxDepth(tree))
